b8aa669b9df8e4a5804ae5b881c0e0f1ded5d87e
Duskt <30536860+Duskt@users.noreply.github.com>
2024-04-13 13:10:07 +0100

    pygame boilerplate

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-# run the game
+'''
+the main script
+'''
+import pygame
+import game
+
+# pygame setup
+pygame.init()
+screen = pygame.display.set_mode((1280, 720))
+clock = pygame.time.Clock()
+running = True
+
+game.run(screen, clock)
--- node.py
+++ node.py
@@ -1,10 +1,13 @@
+''' do maths'''
 from dataclasses import dataclass
 
-# just lets  
 @dataclass
 class Node:
     x: float
     y: float
     z: float
 
-Node(0, 0, 0)
+@dataclass
+class Sphere(Node):
+    radius: float
+
