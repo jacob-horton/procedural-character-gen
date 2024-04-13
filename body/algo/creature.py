8828e663f754b27eff83243ca9f521cc3717af9c
Duskt <30536860+Duskt@users.noreply.github.com>
2024-04-13 21:39:14 +0100

    modularise creature

--- body/algo/creature.py
+++ body/algo/creature.py
@@ -2,42 +2,47 @@
 import random
 from typing import Any
 
-from pygame import Vector3
+from pygame import Vector3, Surface
+import pygame
+from body.polygon import Polygon
 
 class Blob:
-    def __init__(self, node: Vector3, gg: 'Creature', n: int = 3, initial_distance: int = 10):
-        self.gg = gg
-        self.pos = (node.x, node.y, node.z)
-        self.points = gg.make_points(self, n, initial_distance)
-
+    def __init__(self, middle: Vector3, points: list[Vector3]):
+        self.middle = middle
+        self.points = points
 
 class Creature:
     def __init__(self, seed: Any = None):
         self.SEED = seed
         self.RANDOM_STATE = random.Random(self.SEED)
         self.points: list[Vector3] = []
+        self.blobs: list[Blob] = []
+
+    def create_blob(self, middle: Vector3, n: int = 3, initial_distance: int = 10):
+        points = self.make_points(middle, n, initial_distance)
+        self.points += points
+        self.blobs.append(Blob(middle, points))
 
     # generate nodes close by randomly
-    def make_points(self, root: Blob, n: int, initial_distance: int):
+    def make_points(self, middle: Vector3, n: int, initial_distance: int):
         def mk_pt():
-            r = []
+            r: list[float] = []
             for i in range(3):
                 # from [-1, 1)
                 x = (self.RANDOM_STATE.random()*2-1)
                 # from [-10, 10)
                 x *= initial_distance
                 # offset to root +- 10
-                x += root.pos[i]
+                x += middle[i]
                 r.append(x)
 
             return Vector3(r)
         
         point_list = [mk_pt() for _ in range(n)]
-        self.points += point_list
         return point_list
 
     def grow(self, rate: float = 10, repulsion: float = 0.1):
-        for index, point in enumerate(self.points):
+        for point in self.points:
             '''
             Each of the nodes can be considered a vector from (0,0,0), which is the root node.
             We can increase the magnitude of this vector to make them further away, so we do this each step with mods:
@@ -68,3 +73,9 @@ class Creature:
             # add the movement vector to a weighted nearest neighbour vector (nnv)
             movement_vec += nearest_neighbour_vector * repulsion
             point += (movement_vec.normalize()) * rate
+
+    def render(self, screen: Surface):
+        for i in self.blobs:
+            polygon = Polygon.from_3d_points(i.points)
+            polygon.pos = i.middle
+            polygon.draw(screen, pygame.Color(50, 180, 255))
